e7a645a22e4c8be6b78a87413ffa7439a946686d
shaiannesunagawa <sys74530@creighton.edu>
2021-02-24 16:12:29 -0600

    Code w/o withSal variable or removal

---
+++ forms/pets/pets.js
@@ -0,0 +1,5 @@
+let petNames = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam']
+// let withSal = [petNames]
+// withSal.push('Sal')
+console.log(`The third pet is named ${petNames[2]}`)
+console.log(`The array has ${petNames.length} names in it.`)
